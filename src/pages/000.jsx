482eb41d9dc8f797c98af0749ab5b20f065c082c
Che <johnccreations21@gmail.com>
2025-08-19 17:18:30 -0400

    second update

--- src/pages/000.jsx
+++ src/pages/000.jsx
@@ -10,7 +10,7 @@ const Page1 = () => {
     }, []);
 
     return (
-        <body className="main__intro__page">
+        <div className="main__intro__page">
             <div className="nav__row">
                 <div className="navbar__left">
                     <Link className="navbar__left--name" to="/">SKINSTRIC</Link>
@@ -18,9 +18,16 @@ const Page1 = () => {
                     <p className="navbar__left--page-name">INTRO</p>
                     <img className="navbar__left--img" src={RightBracket} alt="" />
                 </div>
-                <button className="navbar__enter-code">ENTER CODE</button>
+                <button className="inline-flex items-center justify-center gap-2 whitespace-nowrap font-semibold transition-colors  disabled:pointer-events-none text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2 mx-4 scale-[0.8] text-[#FCFCFC] text-[10px] bg-[#1A1B1C] leading-[16px]">ENTER CODE</button>
             </div>
-        </body>
+            <div className="max-sm:scale-[0.75] max-sm:origin-center max-sm:p-6">
+                <div className="flex flex-col items-center justify-center h-[71dvh] md:fixed md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2">
+                    <div className="absolute insert-0 flex items-center justify-center lg:hidden">
+                        <div className="w-[350px] h-[350px] border border-dotted border-[#A0A4AB] rotate-45 absolute top-1/2 left-1/2 -translate-x-[52%] -translate-y-1/2"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     );
 };
