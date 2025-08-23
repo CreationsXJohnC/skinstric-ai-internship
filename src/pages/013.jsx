748c3003641e882a74486ff1bb5ca18f1529b1d5
Che <johnccreations21@gmail.com>
2025-08-23 16:19:35 -0400

    ninth update

--- src/pages/013.jsx
+++ src/pages/013.jsx
@@ -20,7 +20,49 @@ const Page6 = () => {
                 </div>
                 <button className="inline-flex items-center justify-center gap-2 whitespace-nowrap font-semibold transition-colors  disabled:pointer-events-none text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2 mx-4 scale-[0.8] text-[#FCFCFC] text-[10px] bg-[#1A1B1C] leading-[16px]">ENTER CODE</button>
             </div>
-            
+            <div className="h-screen md:h-[90px] flex flex-col md:mt-5">
+                <main className="flex-1 w-full bg-white md:overflow-hidden overflow-auto">
+                    <div className="md:h-full max-w-full mx-5 px-4 md:px-auto flex flex-col">
+                        <div className="text-start ml-4 mb-4 md:mb-10 md:ml-0">
+                            <h2 className="text-base md:text-base font-semibold mb-1 leading-[24px]">A.I. ANALYSIS</h2>
+                            <h3 className="text-4xl md:text-[72px] font-normal leading-[64px] tracking-tighter">DEMOGRAPHICS</h3>
+                            <h4 className="text-sm mt-2 leading-[24px]">PREDICTED RACE & AGE</h4>
+                        </div>
+                        <div className="grid md:grid-cols-[1.5fr_8.5fr_3.15fr] gap-4 mt-10 mb-40 md:gap-4 pb-0 md:pb-0 md:mb-0">
+                            <div className="bg-white-100 space-y-3 md:flex md:flex-col h-[62%]">
+                                <div className="p-3 cursor-pointer bg-[#F3F3F4] flex-1 flex flex-col justify-between hover:bg-[#E1E1E2] border-t">
+                                    <p className="text-base font-semibold">Latino Hispanic</p>
+                                    <h4 className="text-base font-semibold mb-1">RACE</h4>
+                                </div>
+                                <div className="p-3 cursor-pointer bg-[#1A1B1C] text-white flex-1 flex flex-col justify-between hover:bg-[#E1E1E2] border-t">
+                                    <p className="text-base font-semibold">30-39</p>
+                                    <h4 className="text-base font-semibold mb-1">AGE</h4>
+                                </div>
+                                <div className="p-3 cursor-pointer bg-[#F3F3F4] flex-1 flex flex-col justify-between hover:bg-[#E1E1E2] border-t">
+                                    <p className="text-base font-semibold">MALE</p>
+                                    <h4 className="text-base font-semibold mb-1">SEX</h4>
+                                </div>
+                            </div>
+                            <div className="relative bg-gray-100 p-4 flex flex-col items-center justify-center md:h-[57vh] md:border-t">
+                                <p className="hidden md:block md:absolute text-[40px] mb-2 left-7 top-4">30-39 y.o.</p>
+                                <div className="relative md:absolute w-full max-w-[384px] aspect-square mb-4 md:right-5 md:bottom-2">
+                                    <div style={{width: "100%", height: "100%", maxheight: "384px", position: "relative", transform: "scale(1)", transformorigin: "center center"}}>
+                                        <svg className="CircularProgressbar-tail text-[#1A1B1C]" viewBox="0 0 100 100" data-test-id="CircularProgressbar">
+                                            <path className="CircularProgressbar-trail" d="M 50,50 m 0,-49.15 a 49.15,49.15 0 1 1 0,98.3 a 49.15,49.15 0 1 1 0,98.3" strokeWidth={1.7} fillOpacity={0} style={{strokeLinecap: "butt", strokeDasharray: "308.819px, 308.819px", strokeDashoffset: "0px"}}></path>
+                                            <path className="CircularProgressbar-path" d="M 50,50 m 0,-49.15 a 49.15,49.15 0 1 1 0,98.3 a 49.15,49.15 0 1 1 0,98.3" strokeWidth={1.7} fillOpacity={0} style={{stroke: "rgb(26, 27, 28)", strokeLinecap: "butt", transitionDuration: "0.8s", strokeDasharray: "308.819px, 308.819px", strokeDashoffset: "308.819px"}}></path>
+                                        </svg>
+                                        <div className="absolute inset-0 flex items-center justify-center">
+                                            <p className="text-3xl md:text-[40px] font-normal">0 <span className="absolute text-xl md:text-3xl">%</span></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <p className="md:absolute text-xs text-[#A0A4AB] md:text-sm lg:text-base font-normal mb-1 leading-[24px] md:bottom-[-15%] md:left-[22%] ld:left-[30%] xl:left-[40%] 2xl:left-[45%]">If A.I. estimate us wrong, select the correct one.</p>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </main>
+            </div>
         </div>
 
     );
